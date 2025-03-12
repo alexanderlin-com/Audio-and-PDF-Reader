42e978a868485131d1c6ed8493bcbbc1fd0b2b19
Alexander Lin <115516791+Alex4810@users.noreply.github.com>
2025-03-11 19:51:03 -0700

    Add files via upload

---
+++ Audiobook and Book Reader/Audiobook_and_Book_ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Audiobook_and_Book_ReaderApp.swift
+//  Audiobook and Book Reader
+//
+//  Created by 林阿狗 on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Audiobook_and_Book_ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
